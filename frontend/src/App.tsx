2a08c8f66240c41407cfef82ea92be300185c92d
kelly galliani <kelly_92b@hotmail.com>
2023-09-08 18:38:28 -0300

    feat: update frontend display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,3 @@
-/* import { useState } from "react"; */
-/* import { PaperClipIcon } from "@heroicons/react/20/solid"; */
-import { useEffect, useState } from "react";
-import { api } from "./services/api";
-import Table from "./components/Table";
-import Modal from "./components/Modal";
 import FileUpload from "./components/FileUpload";
 
 /* const user = {
@@ -13,92 +7,45 @@ import FileUpload from "./components/FileUpload";
     "https://ibb.co/zVwLYX3",
 }; */
 
-export interface iProduct {
-  header: string[];
-  content: string[];
-}
-
 export default function App() {
-  const [showModal, setShowModal] = useState(false);
-  const [products, setProducts] = useState<iProduct>({
-    header: [],
-    content: [],
-  });
-
-  const openModal = (): void => {
-    setShowModal(true);
-  };
-
-  const closeModal = (): void => {
-    setShowModal(false);
-  };
-
-  useEffect(() => {
-    (async () => {
-      const response = await api.get("/");
-      const data = response.data;
-      if (data.length > 0) {
-        const header = Object.keys(data[0]);
-
-        const productsFormatted = data.map(
-          (product: { costPrice: number; salesPrice: number }) => ({
-            ...product,
-            salesPrice: product.salesPrice.toLocaleString("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            }),
-            costPrice: product.costPrice.toLocaleString("pt-BR", {
-              style: "currency",
-              currency: "BRL",
-            }),
-          })
-        );
-
-        setProducts({
-          header,
-          content: productsFormatted,
-        });
-      }
-    })();
-  }, []);
-
   return (
     <>
       <div className="min-h-full">
-        <header className="bg-white shadow">
+        <header className="bg-[#1E2044] shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-[#151A33]">
-              Atualizar Preços
-            </h1>
+            <img
+              src="https://landing.shopper.com.br/static/media/logo-original.c7089ad32bcf61645d35.webp"
+              alt="logo"
+              className="w-24"
+            />
           </div>
         </header>
         <main>
           <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-            <div>
-              <div className="px-4 sm:px-0">
-                <h3 className="text-base font-semibold leading-7 text-gray-900">
-                  Informação de Produtos
+            <div className="px-4 sm:px-0 flex  justify-between">
+              <div>
+                <h3 className="text-3xl font-semibold leading-7 text-gray-900">
+                  Atualizar de Preços em Massa
                 </h3>
                 <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
-                  Lista de Todos os produtos com seus respectivos preços
+                  Envie seu arquivo csv e faça a atualização do preço de seus
+                  produtos
                 </p>
-                {/* <button
-                  className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                  type="button"
-                  onClick={openModal}
-                >
-                  Toggle modal
-                </button> */}
               </div>
-              <div className="mt-6 border-t flex justify-center border-gray-100">
-                {/* <Table products={products} /> */}
-                <FileUpload />
-              </div>
+              <a
+                href="/atualizacao_preco_example.csv"
+                download="modelo_preco_example.csv"
+                className="mt-4 inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-[#27a47d] hover:bg-[#1e8f68] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#27a47d]"
+              >
+                Baixar Modelo de Planilha
+              </a>
+            </div>
+            <div className="mt-6 border-t border-gray-100">
+              <FileUpload />
             </div>
           </div>
         </main>
       </div>
-      <Modal showModal={showModal} closeModal={closeModal} />
     </>
   );
 }
--- frontend/src/components/FileUpload.tsx
+++ frontend/src/components/FileUpload.tsx
@@ -3,10 +3,21 @@ import { useDropzone } from "react-dropzone";
 import { api } from "../services/api";
 import Papa from "papaparse";
 
+interface Produto {
+  product_code: string;
+  new_price: string;
+  erros?: any[];
+}
+
+interface ResultadoParse {
+  data: Produto[];
+  erros?: any[];
+}
+
 const FileUpload = () => {
   const [tableData, setTableData] = useState<[]>([]);
-  const [tableHeaders, setTableHeaders] = useState([]);
-
+  const [tableHeaders, setTableHeaders] = useState<string[]>([]);
+  const [responseType, setResponseType] = useState(0);
   const [typeError, setTypeError] = useState("");
 
   const { getRootProps, getInputProps } = useDropzone({
@@ -18,25 +29,6 @@ const FileUpload = () => {
         return;
       }
 
-      const parseResults = await parseCSV(file);
-      parseResults.erros = [];
-
-      /* if (
-        parseResults.data.length === 0 ||
-        !Object.prototype.hasOwnProperty.call(
-          parseResults.data[0],
-          "new_price"
-        ) ||
-        !Object.prototype.hasOwnProperty.call(
-          parseResults.data[0],
-          "product_code"
-        )
-      ) {
-        renderizarTabela(parseResults.data);
-        
-        return;
-      } */
-
       const formData = new FormData();
       formData.append("file", file);
       try {
@@ -47,47 +39,69 @@ const FileUpload = () => {
         });
 
         const data = response.data;
-
+        setResponseType(1);
         renderizarTabela(data);
         setTypeError("");
-      } catch (error) {
+      } catch (error: any) {
         setTypeError("");
         console.error("Erro ao enviar o arquivo:", error);
-        /* renderizarTabela(parseResults.data); */
+
+        const parseResults = (await parseCSV(file)) as ResultadoParse;
+        parseResults.erros = [];
+
         const apiErrors = error.response.data.errors;
+        const errorsByProduct: { [productCode: string]: { erros: string[] } } =
+          {};
+        let erroNumber = 1;
+
         for (const apiError of apiErrors) {
-          // Encontre os produtos no arquivo CSV com base no productCode
           const produtosComErros = parseResults.data.filter(
             (item) => item.product_code === apiError.productCode
           );
 
-          if (produtosComErros.length > 0) {
-            // Mapeie os produtos com erros e suas mensagens de erro
-            produtosComErros.forEach((product) => {
-              if (!product.erros) {
-                product.erros = [];
-              }
-              product.erros.push(apiError.errorDescription);
-            });
+          for (const product of produtosComErros) {
+            const productCode = product.product_code;
 
-            // Adicione os produtos com erros ao array erros em parseResults
-            parseResults.erros.push(...produtosComErros);
+            if (!errorsByProduct[productCode]) {
+              errorsByProduct[productCode] = {
+                erros: [],
+              };
+            }
+
+            errorsByProduct[productCode].erros.push(
+              `${erroNumber}. ${apiError.errorDescription}`
+            );
+
+            erroNumber++;
           }
         }
 
-        // Renderize o novo array com erros no renderizarTabela
+        for (const product of parseResults.data) {
+          const productCode = product.product_code;
+          if (errorsByProduct[productCode]) {
+            product.erros = errorsByProduct[productCode].erros.map(
+              (error: any) => <div>{error}</div>
+            );
+          }
+        }
+
+        parseResults.erros = parseResults.data.filter(
+          (product) => product.erros
+        );
+
+        setResponseType(2);
         renderizarTabela(parseResults.erros);
       }
     },
   });
 
-  const parseCSV = (file) => {
+  const parseCSV = (file: any) => {
     return new Promise((resolve) => {
       Papa.parse(file, {
         header: true,
         dynamicTyping: true,
         complete: (results) => {
-          resolve({ data: results.data }); // Removido o mapeamento com lineNumber
+          resolve({ data: results.data });
         },
         error: (error) => {
           resolve({ error });
@@ -96,7 +110,7 @@ const FileUpload = () => {
     });
   };
 
-  const renderizarTabela = (data) => {
+  const renderizarTabela = (data: any) => {
     if (data.length > 0) {
       const headers = Object.keys(data[0]);
       setTableHeaders(headers);
@@ -105,45 +119,50 @@ const FileUpload = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full max-w-md m-b">
-      <div
-        {...getRootProps()}
-        className="flex flex-col items-center justify-center w-full h-48 border-2 border-green-500 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-green-200/60"
-      >
-        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg
-            className="w-8 h-8 mb-4 text-gray-500"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
-          <p className="mb-2 text-sm text-gray-500">
-            <span className="font-semibold">
-              Clique para fazer o upload do arquivo CSV
-            </span>{" "}
-            ou arraste e solte
-          </p>
-          <p className="text-xs text-gray-500">CSV (MAX. 1MB)</p>
+    <div className="w-full flex flex-col gap-6">
+      <div className="flex  flex-col items-center justify-center ">
+        <div
+          {...getRootProps()}
+          className="flex flex-col max-w-md items-center justify-center w-full h-32 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-gray-200/60"
+        >
+          <div className="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg
+              className="w-8 h-8 mb-4 text-gray-500"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 16"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+              />
+            </svg>
+            <p className="mb-2 text-sm text-gray-500">
+              <span className="font-semibold">
+                Clique para fazer o upload do arquivo CSV
+              </span>{" "}
+              ou arraste e solte
+            </p>
+            <p className="text-xs text-gray-500">.CSV (MAX. 1MB)</p>
+          </div>
+          <input {...getInputProps()} className="hidden" />
         </div>
-        <input {...getInputProps()} className="hidden" />
+
+        {typeError && (
+          <div className="mt-4 text-red-500 font-semibold">{typeError}</div>
+        )}
       </div>
 
-      {typeError && (
-        <div className="mt-4 text-red-500 font-semibold">{typeError}</div>
-      )}
-
-      {tableData.length > 0 && (
+      {responseType === 1 && (
         <div className="relative overflow-x-auto rounded-lg">
-          <table className="w-full text-sm text-left text-gray-500">
+          <h2 className="text-lg font-semibold text-green-800 mb-7">
+            ✅ Tabela de Preços atualizada com sucesso
+          </h2>
+          <table className="w-full text-sm text-left text-gray-600 ">
             <thead className="text-xs text-gray-800 uppercase bg-[#27a47d]">
               <tr>
                 {tableHeaders.map((header, index) => (
@@ -158,8 +177,52 @@ const FileUpload = () => {
                 <tr
                   key={rowIndex}
                   className={`bg-${
-                    rowIndex % 2 === 0 ? "white" : "[#edf0e8]"
-                  } border-b`}
+                    rowIndex % 2 === 0 ? "white" : "[#edf0e8e1]"
+                  } border-b text-left`}
+                >
+                  {tableHeaders.map((header, colIndex) => (
+                    <td
+                      key={colIndex}
+                      className={`px-6 py-4 ${
+                        colIndex === tableHeaders.length - 1 ? "text-right" : ""
+                      }`}
+                    >
+                      {header === "Preço Atual" || header === "Novo Preço"
+                        ? parseFloat(row[header]).toLocaleString("pt-BR", {
+                            style: "currency",
+                            currency: "BRL",
+                          })
+                        : row[header]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+      {responseType === 2 && (
+        <div className="relative overflow-x-auto rounded-lg">
+          <h2 className="text-lg font-semibold text-red-500 mb-7">
+            ⚠ Não foi possível atualizar a tabela
+          </h2>
+          <table className="w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-gray-800 uppercase bg-[#ff5a3d]">
+              <tr>
+                {tableHeaders.map((header, index) => (
+                  <th key={index} scope="col" className="px-6 py-3">
+                    {header}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="border-2 rounded-sm">
+              {tableData.map((row, rowIndex) => (
+                <tr
+                  key={rowIndex}
+                  className={`bg-${
+                    rowIndex % 2 === 0 ? "white" : "[#edf0e8e1]"
+                  } border-b-2`}
                 >
                   {tableHeaders.map((header, colIndex) => (
                     <td
@@ -182,44 +245,3 @@ const FileUpload = () => {
 };
 
 export default FileUpload;
-
-/* import { useDropzone } from "react-dropzone";
-
-const FileUpload = () => {
-  const { getRootProps, getInputProps } = useDropzone();
-
-  return (
-    <div className="flex items-center justify-center w-full max-w-sm">
-      <div
-        {...getRootProps()}
-        className="flex flex-col items-center justify-center w-full h-40 border-2 border-green-500 border-dashed rounded-lg cursor-pointer bg-gray-50 hover:bg-green-200/60"
-      >
-        <div className="flex flex-col items-center justify-center pt-5 pb-6">
-          <svg
-            className="w-8 h-8 mb-4 text-gray-500 "
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 16"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
-            />
-          </svg>
-          <p className="mb-2 text-sm text-gray-500 ">
-            <span className="font-semibold">Clique para fazer o upload</span> ou
-            arraste e solte
-          </p>
-          <p className="text-xs text-gray-500 ">CSV (MAX. 1MB)</p>
-        </div>
-        <input {...getInputProps()} className="hidden" />
-      </div>
-    </div>
-  );
-};
-
-export default FileUpload; */
